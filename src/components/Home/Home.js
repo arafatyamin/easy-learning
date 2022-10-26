23e5ec4244041933de9e33705520b1c67e0e4916
arafatyamin <mdh631987@gmail.com>
2022-10-26 23:01:44 +0600

    setup home componnet

---
+++ src/components/Home/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Home = () => {
+    const allCourse = useLoaderData();
+    return (
+        <div>
+            <p>all course {allCourse.length}</p>
+            <div className="grid grid-cols-3 gap-4 m-8">
+                {
+                    allCourse.map(course => <div course={course}>
+                        <div>
+                            <div className="card  bg-base-100 shadow-xl">
+                                <figure className="px-10 pt-10">
+                                    <img src={course.url} alt="Shoes" className="rounded-xl h-[100px]" />
+                                </figure>
+                                <div className="card-body items-center text-center">
+                                    <h2 className="card-title ">{course.title}</h2>
+                                    <div className="card-actions">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>)
+                }
+            </div>
+            
+        </div>
+    );
+};
+
+export default Home;
