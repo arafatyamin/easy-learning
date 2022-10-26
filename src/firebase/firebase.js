1c3e0b38699f05b0c64a598ce54f195e1a5e19ff
arafatyamin <mdh631987@gmail.com>
2022-10-26 22:53:51 +0600

    install firebase uthentication

---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDilOhtui3xLuRcAIEXQgIrAbJMMUYPGQ",
+  authDomain: "easylearning-3c312.firebaseapp.com",
+  projectId: "easylearning-3c312",
+  storageBucket: "easylearning-3c312.appspot.com",
+  messagingSenderId: "436238571933",
+  appId: "1:436238571933:web:90d3a16d37a562a8d63774"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
